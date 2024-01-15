a00f905e555c0404d428ac718433834623c2a960
haraganda <gabrielchua9@gmail.com>
2024-01-15 11:10:17 +0800

    ils project for sti

---
+++ src/script.js
@@ -0,0 +1,7 @@
+//button function
+const checkbtn = document.querySelector('.button');
+
+checkbtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    window.document.location.href = 'component/dashboard.html';
+})
