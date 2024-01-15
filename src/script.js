953e16d2ea8b4fbc39dce7eede0af1dfe8c2d96b
mikeHenri <edralinmike457@gmail.com>
2024-01-15 20:48:14 +0800

    added fe files

--- src/script.js
+++ src/script.js
@@ -4,4 +4,4 @@ const checkbtn = document.querySelector('.button');
 checkbtn.addEventListener('click', (e) => {
     e.preventDefault()
     window.document.location.href = 'component/dashboard.html';
-})
+});
